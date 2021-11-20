253e2a85acc54d2325aee5227ff4808faf42d91e
Jahid <jahidulhaque767@gmail.com>
2021-11-21 01:49:11 +0600

    complete banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import { Button, Container, Typography } from "@mui/material";
+import BannerBg from "../../../Images/Banner/Frame.png";
+import "./Banner.css";
+import { Box } from "@mui/system";
+
+const alignment = {
+  display: "flex",
+  alignItems: "center",
+};
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner</h2>
-    </div>
+    <Box className="banner-container" sx={{ mt: 2 }}>
+      <Container>
+        <Grid container spacing={2} style={alignment}>
+          <Grid item xs={12} sm={12} md={6}>
+            <Typography
+              variant="h3"
+              sx={{ fontWeight: "bold", color: "#111430" }}
+            >
+              Let's Grow Your <br /> Brand To The <br /> Next Level
+            </Typography>
+            <Typography sx={{ my: 3, textAlign: "justify" }}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              error officiis incidunt nam praesentium harum. Vitae repellendus
+              ea, sint eum voluptates doloribus explicabo illo rem accusantium
+              eius quibusdam accusamus corporis.
+            </Typography>
+            <Button variant="contained">Hire us</Button>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <img src={BannerBg} alt="" style={{ width: "100%" }} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
