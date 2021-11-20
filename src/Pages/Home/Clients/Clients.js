81e1697669643177beab47492f4d235632d6cb0f
Jahid <jahidulhaque767@gmail.com>
2021-11-21 02:09:56 +0600

    client complete

--- src/Pages/Home/Clients/Clients.js
+++ src/Pages/Home/Clients/Clients.js
@@ -1,11 +1,26 @@
+import { Container } from "@mui/material";
 import React from "react";
+import Slack from "../../../Images/Logos/slack.png";
+import Google from "../../../Images/Logos/google.png";
+import Uber from "../../../Images/Logos/uber.png";
+import Netflix from "../../../Images/Logos/netflix.png";
+import Airbnb from "../../../Images/Logos/airbnb.png";
 import "./Clients.css";
+import { Box } from "@mui/system";
 
 const Clients = () => {
   return (
-    <div>
-      <h2>Clients</h2>
-    </div>
+    <Box sx={{ my: 5 }}>
+      <Container>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <img style={{ width: 150, height: 50 }} src={Slack} alt="" />
+          <img style={{ width: 150, height: 50 }} src={Google} alt="" />
+          <img style={{ width: 150, height: 50 }} src={Uber} alt="" />
+          <img style={{ width: 150, height: 50 }} src={Netflix} alt="" />
+          <img style={{ width: 150, height: 50 }} src={Airbnb} alt="" />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Clients from "../Clients/Clients";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Clients></Clients>
     </div>
   );
 };
