8374a97b3166653f1cf9212d1b04c1a03aedaab9
Jahid <jahidulhaque767@gmail.com>
2021-11-21 01:13:58 +0600

    added navigation and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Home from "./Pages/Home/Home/Home";
+import OurPortfolio from "./Pages/OurPortfolio/OurPortfolio";
+import OurTeam from "./Pages/OurTeam/OurTeam";
+import Navigation from "./Pages/Shared/Navigation/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navigation></Navigation>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route
+            path="/portfolio"
+            element={<OurPortfolio></OurPortfolio>}
+          ></Route>
+          <Route path="/team" element={<OurTeam></OurTeam>}></Route>
+          <Route path="/contact" element={<ContactUs></ContactUs>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
