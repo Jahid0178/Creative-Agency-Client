9c5c62d3060d96c253e7128be8b1aeeebe3136c9
Jahid <jahidulhaque767@gmail.com>
2021-11-27 05:00:56 +0600

    added logo in navigation

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -6,72 +6,71 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { NavLink } from "react-router-dom";
+import Logo from "../../../Images/Logos/logo.png";
 
 const Navigation = () => {
   return (
     <Box position="sticky" sx={{ flexGrow: 1, top: 0, zIndex: 999 }}>
       <AppBar position="fixed" sx={{ background: "#FBD062", boxShadow: 0 }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, color: "#333" }}
-          >
-            News
-          </Typography>
-          <NavLink
-            style={{
-              color: "#333",
-              textDecoration: "none",
-              marginLeft: "15px",
-              fontWeight: 500,
-            }}
-            to="/"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            style={{
-              color: "#333",
-              textDecoration: "none",
-              marginLeft: "15px",
-              fontWeight: 500,
-            }}
-            to="/portfolio"
-          >
-            Our Portfolio
-          </NavLink>
-          <NavLink
-            style={{
-              color: "#333",
-              textDecoration: "none",
-              marginLeft: "15px",
-              fontWeight: 500,
-            }}
-            to="/team"
-          >
-            Our Team
-          </NavLink>
-          <NavLink
-            style={{
-              color: "#333",
-              textDecoration: "none",
-              marginLeft: "15px",
-              fontWeight: 500,
-            }}
-            to="/contact"
-          >
-            Contact Us
-          </NavLink>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <img src={Logo} alt="" style={{ width: 120 }} />
+          </Box>
+          <Box>
+            <NavLink
+              style={{
+                color: "#333",
+                textDecoration: "none",
+                marginLeft: "15px",
+                fontWeight: 500,
+              }}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              style={{
+                color: "#333",
+                textDecoration: "none",
+                marginLeft: "15px",
+                fontWeight: 500,
+              }}
+              to="/portfolio"
+            >
+              Our Portfolio
+            </NavLink>
+            <NavLink
+              style={{
+                color: "#333",
+                textDecoration: "none",
+                marginLeft: "15px",
+                fontWeight: 500,
+              }}
+              to="/team"
+            >
+              Our Team
+            </NavLink>
+            <NavLink
+              style={{
+                color: "#333",
+                textDecoration: "none",
+                marginLeft: "15px",
+                fontWeight: 500,
+              }}
+              to="/contact"
+            >
+              Contact Us
+            </NavLink>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
